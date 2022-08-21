833e224b8d7ea8925d4b72072671a99ffe5b7d42
Aaron Marshall <aaronmarshall@MacBook-Air.attlocal.net>
2022-08-21 14:45:04 -0500

    5. Token transfers

--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Token', ()=> {
- let token, accounts, deployer
+ let token, accounts, deployer, receiver
 
 beforeEach(async () => {
 	const Token = await ethers.getContractFactory('Token')
@@ -14,6 +14,7 @@ beforeEach(async () => {
 
 	accounts = await ethers.getSigners()
 	deployer = accounts[0] 
+	receiver = accounts [1]
 })
 	
 describe('Deployment', () => {
@@ -43,5 +44,49 @@ describe('Deployment', () => {
     })
 
 })
+
+
+  describe('Sending Tokens', () => {
+    let amount, transaction, result
+
+    describe('Success', () => {
+
+      beforeEach(async () => {
+        amount = tokens(100)
+        transaction = await token.connect(deployer).transfer(receiver.address, amount)
+        result = await transaction.wait()
+      })
+
+      it('transfers token balances', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+        expect(await token.balanceOf(receiver.address)).to.equal(amount)
+      })
+
+      it('emits a Transfer event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Transfer')
+
+        const args = event.args
+        expect(args.from).to.equal(deployer.address)
+        expect(args.to).to.equal(receiver.address)
+        expect(args.value).to.equal(amount)
+      })
+
+    })
+
+    describe('Failure', () => {
+      it('rejects insufficient balances', async () => {
+        const invalidAmount = tokens(100000000)
+        await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+      })
+
+      it('rejects invalid recipent', async () => {
+        const amount = tokens(100)
+        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      })
+
+    })
+
+  })
 
 })
