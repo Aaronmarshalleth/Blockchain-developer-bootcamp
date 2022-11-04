dc3baa24fa9d55a6686c6cc8819dfbe27462d4b4
Aaron Marshall <aaronmarshall@MacBook-Air.local>
2022-11-04 10:38:52 -0500

    7.Token transfer delegation

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -40,14 +40,23 @@ contract Token {
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
-
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    function _transfer(
+    	address _from,
+    	address _to, 
+    	uint256 _value
+    ) internal {
+        require(_to != address(0));
+
+    	balanceOf[_from] = balanceOf[_from] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(address _spender, uint256 _value) 
@@ -62,11 +71,23 @@ contract Token {
     	return true;
     }
 
+    function transferFrom(
+    	address _from, 
+    	address _to,
+    	uint256 _value
+	) 
+    	public 
+		returns (bool success) 
+	{
+   		require(_value <= balanceOf[_from]);
+    	require(_value <= allowance[_from][msg.sender]);
+    
+    	allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
 
+    	_transfer(_from, _to, _value);
 
-
-
-
+    	return true;
+    }
 
 }
 
