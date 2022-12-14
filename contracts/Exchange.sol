ac5bc224e3168a652f892708c7eb34975fbf7412
Aaron Marshall <aaronmarshall@MacBook-Air.local>
2022-12-14 08:41:05 -0600

    8. Create exchange

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,5 +4,11 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Exchange {
+	address public feeAccount;
+	uint256 public feePercent;
 
+	constructor(address _feeAccount, uint256 _feePercent){
+		feeAccount = _feeAccount;    
+    	feePercent = _feePercent;
+	}
 }
