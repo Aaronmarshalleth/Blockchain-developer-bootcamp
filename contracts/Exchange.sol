216068d2b56030caaa8983fac07d99c8ba0fb98f
Aaron Marshall <aaronmarshall@MacBook-Air.local>
2022-12-20 13:06:24 -0600

    12. Cancel Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -9,12 +9,13 @@ contract Exchange {
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
-    uint256 public orderCount; // 0
+    uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
     event Deposit(
-        address token, 
-        address user, 
-        uint256 amount, 
+        address token,
+        address user,
+        uint256 amount,
         uint256 balance
     );
     event Withdraw(
@@ -24,26 +25,35 @@ contract Exchange {
         uint256 balance
     );
     event Order(
-        uint256 id, 
+        uint256 id,
         address user,
-        address tokenGet, 
-        uint256 amountGet, 
-        address tokenGive, 
-        uint256 amountGive, 
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
         uint256 timestamp
     );
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
 
     struct _Order {
         // Attributes of an order
         uint256 id; // Unique identifier for order
         address user; // User who made order
-        address _tokenGet; // Address of the token they receive
+        address tokenGet; // Address of the token they receive
         uint256 amountGet; // Amount they receive
-        address _tokenGive; // Address of token they give
-        uint256 _amountGive; // Amount they give
-        uint256 timestamp; //When order was created
+        address tokenGive; // Address of token they give
+        uint256 amountGive; // Amount they give
+        uint256 timestamp; // When order was created
     }
-
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -87,41 +97,65 @@ contract Exchange {
         return tokens[_token][_user];
     }
 
-    // -----------------------
+
+    // ------------------------
     // MAKE & CANCEL ORDERS
     function makeOrder(
-        address _tokenGet, 
-        uint256 _amountGet, 
-        address _tokenGive, 
+        address _tokenGet,
+        uint256 _amountGet,
+        address _tokenGive,
         uint256 _amountGive
     ) public {
         // Prevent orders if tokens aren't on exchange
         require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
 
         // Instantiate a new order
-        orderCount = orderCount +1;
-        orders[orderCount] =_Order(
-            orderCount, 
-            msg.sender, 
-            _tokenGet, 
-            _amountGet, 
-            _tokenGive, 
-            _amountGive, 
-            block.timestamp 
+        orderCount = orderCount + 1;
+        orders[orderCount] = _Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
         );
 
-        // EMIT event
-       emit Order(
-            orderCount, 
-            msg.sender, 
-            _tokenGet, 
-            _amountGet, 
-            _tokenGive, 
-            _amountGive, 
-            block.timestamp 
+        // Emit event
+        emit Order(
+            orderCount,
+            msg.sender,
+            _tokenGet,
+            _amountGet,
+            _tokenGive,
+            _amountGive,
+            block.timestamp
         );
     }
 
+    function cancelOrder(uint256 _id) public {
+        // Fetch order
+        _Order storage _order = orders[_id];
 
+        // Ensure the caller of the function is the owner of the order
+        require(address(_order.user) == msg.sender);
+
+        // Order must exist
+        require(_order.id == _id);
+
+        // Cancel the order
+        orderCancelled[_id] = true;
+
+        // Emit event
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
+    }
 
 }
