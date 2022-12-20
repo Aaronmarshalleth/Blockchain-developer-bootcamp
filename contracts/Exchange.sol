ca870bd8075dadd6f311686b1f49082cd12fc846
Aaron Marshall <aaronmarshall@MacBook-Air.local>
2022-12-20 11:39:11 -0600

    11. Make Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -8,14 +8,42 @@ contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    uint256 public orderCount; // 0
 
-    event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Deposit(
+        address token, 
+        address user, 
+        uint256 amount, 
+        uint256 balance
+    );
     event Withdraw(
         address token,
         address user,
         uint256 amount,
         uint256 balance
     );
+    event Order(
+        uint256 id, 
+        address user,
+        address tokenGet, 
+        uint256 amountGet, 
+        address tokenGive, 
+        uint256 amountGive, 
+        uint256 timestamp
+    );
+
+    struct _Order {
+        // Attributes of an order
+        uint256 id; // Unique identifier for order
+        address user; // User who made order
+        address _tokenGet; // Address of the token they receive
+        uint256 amountGet; // Amount they receive
+        address _tokenGive; // Address of token they give
+        uint256 _amountGive; // Amount they give
+        uint256 timestamp; //When order was created
+    }
+
 
     constructor(address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -58,5 +86,42 @@ contract Exchange {
     {
         return tokens[_token][_user];
     }
+
+    // -----------------------
+    // MAKE & CANCEL ORDERS
+    function makeOrder(
+        address _tokenGet, 
+        uint256 _amountGet, 
+        address _tokenGive, 
+        uint256 _amountGive
+    ) public {
+        // Prevent orders if tokens aren't on exchange
+        require(balanceOf(_tokenGive, msg.sender) >= _amountGive);
+
+        // Instantiate a new order
+        orderCount = orderCount +1;
+        orders[orderCount] =_Order(
+            orderCount, 
+            msg.sender, 
+            _tokenGet, 
+            _amountGet, 
+            _tokenGive, 
+            _amountGive, 
+            block.timestamp 
+        );
+
+        // EMIT event
+       emit Order(
+            orderCount, 
+            msg.sender, 
+            _tokenGet, 
+            _amountGet, 
+            _tokenGive, 
+            _amountGive, 
+            block.timestamp 
+        );
+    }
+
+
 
 }
