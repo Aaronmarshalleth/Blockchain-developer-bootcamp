8270e427bd6d8d7170eb11724999c8d6b5ea944a
Aaron Marshall <aaronmarshall@MacBook-Air.local>
2023-01-05 09:44:45 -0600

    14.Seed Exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,11 +1,31 @@
 async function main() {
-  // Fetch contract to deploy
-  const Token = await ethers.getContractFactory("Token")
+  console.log(`Preparing deployment...\n`)
 
-  // Deploy contract
-  const token = await Token.deploy()
-  await token.deployed()
-  console.log(`Token Deployed to: ${token.address}`)
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory('Token')
+  const Exchange = await ethers.getContractFactory('Exchange')
+
+  // Fetch accounts
+  const accounts = await ethers.getSigners()
+
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
+
+  // Deploy contracts
+  const dapp = await Token.deploy('Dapp University', 'DAPP', '1000000')
+  await dapp.deployed()
+  console.log(`DAPP Deployed to: ${dapp.address}`)
+
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000')
+  await mETH.deployed()
+  console.log(`mETH Deployed to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000')
+  await mDAI.deployed()
+  console.log(`mDAI Deployed to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange Deployed to: ${exchange.address}`)
 }
 
 main()
