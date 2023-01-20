14cac3b3fb1b43c7468fdb7e865727de1d7211a0
Aaron Marshall <aaronmarshall@MacBook-Air.local>
2023-01-20 09:40:13 -0600

    22. Withdrawals

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -55,6 +55,10 @@ export const subscribeToEvents = (exchange, dispatch) => {
   exchange.on('Deposit', (token, user, amount, balance, event) => {
     dispatch({ type: 'TRANSFER_SUCCESS', event })
   })
+
+  exchange.on('Withdraw', (token, user, amount, balance, event) => {
+    dispatch({ type: 'TRANSFER_SUCCESS', event })
+  })
 }
 
 // ------------------------------------------------------------------------------
@@ -88,9 +92,13 @@ export const transferTokens =  async (provider, exchange, transferType, token, a
     const signer = await provider.getSigner()
     const amountToTransfer = ethers.utils.parseUnits(amount.toString(), 18)
 
-    transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
-    await transaction.wait()
-    transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+    if (transferType === 'Deposit') {
+      transaction = await token.connect(signer).approve(exchange.address, amountToTransfer)
+      await transaction.wait()
+      transaction = await exchange.connect(signer).depositToken(token.address, amountToTransfer)
+    } else {
+      transaction = await exchange.connect(signer).withdrawToken(token.address, amountToTransfer)
+    }
 
     await transaction.wait()
 
@@ -98,3 +106,4 @@ export const transferTokens =  async (provider, exchange, transferType, token, a
     dispatch({ type: 'TRANSFER_FAIL' })
   }
 }
+
